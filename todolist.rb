c16ae283a1e84a381583741fb10eecb5dd037c09
Ryan McIntire <ryanmcin@gmail.com>
2016-10-23 14:58:54 -0700

    Add todolist.

---
+++ todolist.rb
@@ -0,0 +1,12 @@
+#todo - styling on user profile page.
+#todo - Styling of jurisdiction dropdown on new question page.
+#todo - add tags to question.
+#todo - Possible links on new question?
+#todo - user picture?
+#todo - link to firm profile?
+#todo - better question list on landing page.
+#todo - better handling of vote click in votebox component.
+#todo - "Other items here...."
+#todo - We'll add advertisements, related questions, and top question in jdx.
+#todo - Across-site responsive design
+#todo - Add model validations
