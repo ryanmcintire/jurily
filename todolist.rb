7617dd84ab4c3dcf38d66ad65db661f59b055993
Ryan McIntire <ryanmcin@gmail.com>
2016-12-31 14:47:37 -0800

    Miscellaneous improvements.

--- todolist.rb
+++ todolist.rb
@@ -1,20 +1,12 @@
-#todo - edit signup email sender
+
 #todo - add tags to question.
 #todo - Possible links on new question?
 #todo - user picture?
-#todo - better question list on landing page.
-#todo - better handling of vote click in votebox component.
 #todo - We'll add advertisements, related questions, and top question in jdx.
-#todo - Across-site responsive design
 #todo - Add model validations
-#todo - fix pagination for first page....
-#todo - flash confirmation for confirmable.
-#todo - change confirmation message.
 #todo - Validation of question form.
-#todo - configure webservers in environments
-#todo - Add ability to edit question.
+#todo - edit answers
 #todo - manage employment w/o history (simple update system)0
-#todo - Add answer to rich text editof ro answer.
 #todo - Wrap text if edit appears in answer and in question.
 #todo - force json error response for json api endpoints
 #todo - Add "member since" to user profile title section
@@ -30,3 +22,5 @@
 #todo - seed database
 #todo - unit tests.
 #todo - jurisdiction link gets questions based on jurisdiction
+# ===========================================
+#todo - edit signup email sender
