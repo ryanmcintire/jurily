e26834645536de6cb4dcd0467fbd82629f5c6368
Ryan McIntire <ryanmcin@gmail.com>
2016-12-30 16:54:09 -0800

    Configure ActionMailer for sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => 'https://jurily-development.herokuapp.com/', :protocol => 'https'}
+  config.action_mailer.default_url_options = {:host => 'http://www.jurily.com', :protocol => 'https'}
 end
