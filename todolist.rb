2f4438460902b22edcbf4d0cde927485ada98805
Ryan McIntire <ryanmcin@gmail.com>
2016-12-31 18:10:33 -0800

    Remove adds column for time being.

--- todolist.rb
+++ todolist.rb
@@ -1,30 +1,24 @@
-#todo - fix answer ordering - order by score!
 #todo - prevent vote from even tabulating locally if not logged in.
-
-
-
 #todo - add tags to question.
-#todo - Possible links on new question?
-#todo - user picture?
+
+#todo - Figure out top answers bug.
+
 #todo - We'll add advertisements, related questions, and top question in jdx.
 #todo - Add model validations
 #todo - Validation of question form.
 #todo - edit answers
-#todo - manage employment w/o history (simple update system)0
-#todo - Wrap text if edit appears in answer and in question.
 #todo - force json error response for json api endpoints
 #todo - Add "member since" to user profile title section
-#todo - sort answers in order highest score to lowest.
-#todo - enable editing answers / questions
-#todo - load votes on page load.
 #todo - db checks to make sure user votes only once.
-#todo - pluralize votes on home page.
-#todo - flash confirmation email message on signup.
 #todo - change successful confirmation to green text.
 #todo - attempt to submit answer redirects if not logged in.
 #todo - proper styling for phone display.
 #todo - seed database
-#todo - unit tests.
 #todo - jurisdiction link gets questions based on jurisdiction
+
 # ===========================================
+
+#todo - Possible links on new question?
+#todo - user picture?
+
 #todo - edit signup email sender
